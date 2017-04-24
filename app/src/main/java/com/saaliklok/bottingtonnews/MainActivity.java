727ad31105f2c1571929875646412513f678417a
Saalik Lokhandwala <saaliklok@Saaliks-MacBook-Air.local>
2017-04-24 12:40:43 -0400

    UI is shakey at best on this branch

--- app/src/main/java/com/saaliklok/bottingtonnews/MainActivity.java
+++ app/src/main/java/com/saaliklok/bottingtonnews/MainActivity.java
@@ -2,8 +2,18 @@ package com.saaliklok.bottingtonnews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView messagesContain;
+    private Adapter adapter;
+    private ArrayAdapter<String> messageHistory;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/saaliklok/bottingtonnews/MessageAdapter.java
@@ -0,0 +1,20 @@
+package com.saaliklok.bottingtonnews;
+
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+/**
+ * Created by saaliklok on 4/22/17.
+ */
+
+/*
+public class MessageAdapter extends ArrayAdapter {
+
+    public MessageAdapter(){
+
+    }
+
+
+}
+*/
